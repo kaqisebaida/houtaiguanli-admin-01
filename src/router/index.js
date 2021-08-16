8ecab9b7e0f819952c6978a76eb031250582dea7
卡其色百搭 <2030416394@qq.com>
2021-08-16 15:29:02 +0800

    添加修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/login'
-import Home from '@/views/home'
-import Layout from '@/views/layout'
-import Article from '@/views/article'
-import Punlish from '@/views/punlish'
-import Image from '@/views/image'
-import Comment from '@/views/comment'
-import Settings from '@/views/settings'
-import Fans from '@/views/fans'
+
+// 路由懒加载
+const Login = () => import('../views/login')
+const Home = () => import('@/views/home')
+const Layout = () => import('@/views/layout')
+const Article = () => import('@/views/article')
+const Punlish = () => import('@/views/punlish')
+const Image = () => import('@/views/image')
+const Comment = () => import('@/views/comment')
+const Settings = () => import('@/views/settings')
+const Fans = () => import('@/views/fans')
+// import Login from '../views/login'
+// import Home from '@/views/home'
+// import Layout from '@/views/layout'
+// import Article from '@/views/article'
+// import Punlish from '@/views/punlish'
+// import Image from '@/views/image'
+// import Comment from '@/views/comment'
+// import Settings from '@/views/settings'
+// import Fans from '@/views/fans'
 
 Vue.use(VueRouter)
 
